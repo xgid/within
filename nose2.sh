e7406ad5bac0d7bfb82304409d7f68ee2486cfad
Brendan Curran-Johnson <brendan@bcjbcj.ca>
2014-06-29 09:54:26 -0500

    adding TravisCI CoverAlls

--- nose2.sh
+++
@@ -1,6 +0,0 @@
-set -e
-
-echo '---installing---'
-python2.7 setup.py install
-echo '---testing---'
-nosetests-2.7 --with-coverage --cover-package within tests -v
--- nose3.sh
+++
@@ -1,6 +0,0 @@
-set -e
-
-echo '---installing---'
-python3.4 setup.py install
-echo '---testing---'
-nosetests-3.4 --with-coverage --cover-package within tests -v
